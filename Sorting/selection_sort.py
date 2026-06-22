04982f9a14aa5813af5a892db4a9cf7764ef5462
Purvi Tyagi <pt8495044@gmail.com>
2026-06-22 21:55:10 +0530

    sorting updated

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -1,4 +1,4 @@
-#selection sort
+#selection sort ascending order
 nums=[1,7,8,4,5,6,9,2]
 n=len(nums)
 for i in range(n):
@@ -8,3 +8,14 @@ for i in range(n):
             min_indx=j
     nums[i],nums[min_indx]=nums[min_indx],nums[i]
 print(nums)
+nums=[1,7,8,4,5,6,9,2]
+
+#descending order
+for i in range(n):
+    max_indx=i
+    for j in range(i+1,n):
+        if nums[j]>nums[max_indx]:
+            max_indx=j
+    nums[i],nums[max_indx]=nums[max_indx],nums[i]
+print(nums)
+    
