c341e825ee9ffad77ef11d14eb7706fb87f0f218
Purvi Tyagi <pt8495044@gmail.com>
2026-06-22 18:13:14 +0530

    optimized LC 424

--- Sliding_Window/LC  424.py
+++ Sliding_Window/LC  424.py
@@ -7,20 +7,16 @@ class Solution(object):
          l=0
          r=0
          win_len_max=0
+         max_freq=0
          for r in range(len(s)):
-              win_len=r-l+1     
-              count[s[r]]=count.get(s[r],0)+1
-              diff=win_len-max(count.values())
-              while diff>k:
-                   count[s[l]]-=1
-                   if count[s[l]]==0:
-                        del count[s[l]]
-                   l+=1
-                   win_len=r-l+1     
-                   diff=win_len-max(count.values())
-              if diff<=k:
-                   win_len_max=max(win_len_max,r-l+1)
-         return win_len_max         
+            count[s[r]]=count.get(s[r],0)+1
+            max_freq=max(max_freq,count[s[r]])
 
+            
+            while (r-l+1)-max_freq>k:
+                count[s[l]]-=1
+                l+=1
+            win_len_max=max(win_len_max,r-l+1)
+         return win_len_max
 ss=Solution()
 print(ss.characterReplacement(s,k))
