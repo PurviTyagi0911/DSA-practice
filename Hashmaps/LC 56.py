04974c946af14b3c73824778a119f12ae061e6f4
Purvi Tyagi <pt8495044@gmail.com>
2026-06-26 17:49:56 +0530

    optimized LC 56

--- Hashmaps/LC 56.py
+++ Hashmaps/LC 56.py
@@ -1,22 +1,22 @@
 #Merge Intervals
-intervals = [[1,8],[0,6]]
+intervals = [[1,8],[7,10]]
 class Solution(object):
     def merge(self, intervals):
         intervals=sorted(intervals)
         output=[]
-        start=intervals[0][0]
-        end=intervals[0][1]
-        output.append(intervals[0])
-        for s in intervals:
-                if s[0] in range(start,end+1):
-                    output.pop(-1)
-                    end=max(s[1],end)
-                    output.append([start,end])
-                    print('extend')
-                else:
-                     start=s[0]
-                     end=s[1]
-                     output.append([start,end])
+        start,end=intervals[0]
+
+        for curr_start, curr_end in intervals[1:]:
+               if curr_start<=end:
+                    end=max(curr_end,end)
+               else:
+                 output.append([start,end])
+                 start,end=curr_start,curr_end
+        output.append([start,end])
+                    
+                    
+
+               
                 
         return output  
             
