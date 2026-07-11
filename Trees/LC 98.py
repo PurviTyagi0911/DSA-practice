e4ce3fb8b111b3ad87ea0156b7474aff39632c4e
Purvi Tyagi <pt8495044@gmail.com>
2026-07-11 23:51:39 +0530

    lc 98 updated

--- Trees/LC 98.py
+++ Trees/LC 98.py
@@ -5,23 +5,24 @@ class Node:
         self.right=None
         self.left=None
 root=Node(2) 
-zero=Node(0)
+zero=Node(3)
 one=Node(1)
 root.left=one
 root.right=zero
 class Solution(object):
+    min=-10
+    max=0
     def isValidBST(self, root):
-        if root==None:
-            return True
-        if root.left!=None and root.left.val>=root.val:
-            return False
-        if root.right!=None and root.right.val<=root.val:
-            return False
-        a=self.isValidBST(root.left)
-        b=self.isValidBST(root.right)
-        if a == b== True:
-            return True
-        else:
-            return False
+        def dfs(node, low, high):
+            if node is None:
+                return True
+
+            if node.val <= low or node.val >= high:
+                return False
+
+            return (dfs(node.left, low, node.val) and
+                    dfs(node.right, node.val, high))
+        return dfs(root, float("-inf"), float("inf"))
+
 ss=Solution()
 print(ss.isValidBST(root))
